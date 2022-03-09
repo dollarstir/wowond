8ed53e0cb11d58f506c751e5a853b2cefdc4af58
dollarstir <kpin463@gmail.com>
2022-03-09 07:00:16 +0000

    wowonder

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2017 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+@header('Location: install');
+exit();
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "script";
+
+// Site URL
+$site_url = "http://localhost/script"; // e.g (http://example.com)
